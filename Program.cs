7fa04315a946d34bee515a4debca435a1f037748
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-03-18 11:03:51 +0530

    Welcome to the Employee Payroll Service ADO.Net

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Payroll Service ADO.Net");
+        }
+    }
+}
