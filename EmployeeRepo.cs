d6b6c6953a1e8572ad75ca6140bff22ffaad26be
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-19 17:57:56 +0530

    Added: Cascading Delete

---
+++ EmployeeRepo.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollService
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public bool AddEmployee(EmployeeModel model)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails", this.connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", model.EmployeeName);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@Department", model.Department);
+                    command.Parameters.AddWithValue("@Gender", model.Gender);
+                    command.Parameters.AddWithValue("@BasicPay", model.BasicPay);
+                    command.Parameters.AddWithValue("@Deductions", model.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", model.TaxablePay);
+                    command.Parameters.AddWithValue("@Tax", model.Tax);
+                    command.Parameters.AddWithValue("@NetPay", model.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", DateTime.Now);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@Country", model.Country);
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
+
+        public void GetAllEmployee()
+        {
+            try
+            {
+                EmployeeModel employeeModel = new EmployeeModel();
+                using (this.connection)
+                {
+                    string query = @"Select EmployeeID,EmployeeName,PhoneNumber,Address,Department,Gender,BasicPay,Deductions,TaxablePay,Tax,NetPay," +
+                        "StartDate,City,Country from employee";
+
+                    //string query = @"Select * from employee";
+
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
+
+                    this.connection.Open();
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            employeeModel.EmployeeID = dr.GetInt32(0);
+                            employeeModel.EmployeeName = dr.GetString(1);
+                            employeeModel.PhoneNumber = dr.GetString(2);
+                            employeeModel.Address = dr.GetString(3);
+                            employeeModel.Department = dr.GetString(4);
+                            employeeModel.Gender = Convert.ToChar(dr.GetString(5));
+                            employeeModel.BasicPay = dr.GetDouble(6);
+                            employeeModel.Deductions = dr.GetDouble(7);
+                            employeeModel.TaxablePay = dr.GetDouble(8);
+                            employeeModel.Tax = dr.GetDouble(9);
+                            employeeModel.NetPay = dr.GetDouble(10);
+                            employeeModel.StartDate = dr.GetDateTime(11);
+                            employeeModel.City = dr.GetString(12);
+                            employeeModel.Country = dr.GetString(13);
+
+                            Console.WriteLine("{0} {1} {2} {3}  ", employeeModel.EmployeeID, employeeModel.EmployeeName, employeeModel.PhoneNumber, employeeModel.Address);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Data Not Found");
+                    }
+                    dr.Close();
+                    this.connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        public void UpdateSalaryQuery()
+        {
+            try
+            {
+                EmployeeModel employeeModel = new EmployeeModel();
+                using (this.connection)
+
+                {
+                    //sqlConnection.Open();
+                    string query = "update employee set BasicPay=3000000 where EmployeeName= 'Bruce wayne'";
+                    this.connection.Open();
+                    //Pass query to TSql
+                    SqlCommand sqlCommand = new SqlCommand(query, this.connection);
+                    int result = sqlCommand.ExecuteNonQuery();
+                    if (result != 0)
+                    {
+                        Console.WriteLine("Updated!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Updated!");
+                    }
+                    //Close Connection
+                    this.connection.Close();
+                    GetAllEmployee();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        public int UpdateSalary(EmployeeModel employeeDataModel)
+        {
+            int result = 30000000;
+            try
+            {
+                using (this.connection)
+                {
+                    //Give stored Procedure
+                    SqlCommand sqlCommand = new SqlCommand("dbo.spUpdateSalary", this.connection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@salary", employeeDataModel.BasicPay);
+                    sqlCommand.Parameters.AddWithValue("@EmpName", employeeDataModel.EmployeeName);
+                    sqlCommand.Parameters.AddWithValue("@EmpId", employeeDataModel.EmployeeID);
+                    //Open Connection
+                    connection.Open();
+                    //Return Number of Rows affected
+                    result = sqlCommand.ExecuteNonQuery();
+                    if (result != 0)
+                    {
+                        Console.WriteLine("Updated");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Updated");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+        public string DataBasedOnDateRange()
+        {
+            string nameList = "";
+            try
+            {
+                using (this.connection)
+                {
+                    //query execution
+                    string query = @"select * from employee_payroll where StartDate BETWEEN Cast('2019-12-13' as Date) and GetDate();";
+                    SqlCommand command = new SqlCommand(query, this.connection);
+                    //open sql connection
+                    connection.Open();
+
+                    SqlDataReader sqlDataReader = command.ExecuteReader();
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            GetAllEmployee();
+                            nameList += sqlDataReader["EmployeeName"].ToString() + " ";
+                        }
+                    }
+                    //close reader
+                    sqlDataReader.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+                connection.Close();
+            }
+            //returns the count of employee in the list between the given range
+            return nameList;
+
+        }
+        public string AggregateFunctionBasedOnGender(string query)
+        {
+            string nameList = "";
+            try
+            {
+                using (this.connection)
+                {
+                    ////query execution
+                    SqlCommand command = new SqlCommand(query, this.connection);
+                    //open sql connection
+                    connection.Open();
+
+                    SqlDataReader sqlDataReader = command.ExecuteReader();
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine("TotalSalary: {0} \t MinimumSalary: {1} \t MaximumSalary: {2}AverageSalary: {3} \t Count: {4}", sqlDataReader[0], sqlDataReader[1], sqlDataReader[2], sqlDataReader[3], sqlDataReader[4]);
+                            nameList += sqlDataReader[0] + " " + sqlDataReader[1] + " " + sqlDataReader[2] + " " + sqlDataReader[3] + " " + sqlDataReader[4];
+                        }
+                    }
+                    //close reader
+                    sqlDataReader.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+                connection.Close();
+            }
+            //returns the count of employee in the list between the given range
+            return nameList;
+
+        }
+
+
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,56 @@
+using EmployeePayrollService;
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("Using Stored Procedure")]
+        public void GivenUpdateQuery_UsingStoredProcedure_ReturnOne()
+        {
+            EmployeeModel employeeDataManager = new EmployeeModel();
+            EmployeeRepo repo = new EmployeeRepo();
+
+            int expected = 30000000;
+            employeeDataManager.EmployeeID = 1;
+            employeeDataManager.EmployeeName = "Bruce wayne";
+            employeeDataManager.BasicPay = 30000000;
+
+            int actual = repo.UpdateSalary(employeeDataManager);
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        [TestCategory("Using SQL Query for Male")]
+        public void GivenGenderMale_GroupBygender_ReturnAggregateFunction()
+        {
+            EmployeeModel employeeDataManager = new EmployeeModel();
+            string expected = "30250000 250000 30000000 15125000 2";
+            string query = "select sum(BasicPay) as TotalSalary,min(BasicPay) as MinimumSalary,max(BasicPay) as MaximumSalary,Round(avg(BasicPay), 0) as AverageSalary,Count(BasicPay) as Count from employee_payroll where Gender = 'M' group by Gender";
+            string actual = EmployeeRepo.AggregateFunctionBasedOnGender(query);
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        [TestCategory("Using Transaction Query")]
+        public void GivenInsertQuery_usingTransaction_returnOne()
+        {
+            int expected = 1;
+            TransactionClass transactionClass = new TransactionClass();
+            int actual = transactionClass.InsertIntoTables();
+            Assert.AreEqual(actual, expected);
+        }
+
+        //Usecase 8.1: Delete using Cascade Delete alteration
+        [TestMethod]
+        [TestCategory("Using Transaction Query")]
+        public void GivenDeleteQuery_usingTransaction_returnOne()
+        {
+            int expected = 1;
+            TransactionClass transactionClass = new TransactionClass();
+            int actual = transactionClass.DeleteUsingCasadeDelete();
+            Assert.AreEqual(actual, expected);
+        }
+    }
+
+}
